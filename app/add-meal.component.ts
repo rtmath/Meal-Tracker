ac675ba0b4c14a6394e818ec93a0728421c4cc18
Ryan Mathisen <ryantmathisen@gmail.com>
2017-01-20 15:58:03 -0800

    add a couple more css tweaks

--- app/add-meal.component.ts
+++ app/add-meal.component.ts
@@ -5,17 +5,25 @@ import { Component, Input, Output, EventEmitter } from '@angular/core';
   selector: "add-meal",
   template: `
     <div class="button" *ngIf="!addNewForm" (click)="toggleFormDisplay()">Add Meal</div>
-    <div *ngIf="addNewForm">
-      <label>Meal Name</label><br>
-      <input #newName type="text"><br>
-      <label>Details</label><br>
-      <input #newDetails type="text"><br>
-      <label>Calories</label><br>
-      <input #newCalories type="number"><br>
-      <div class="button"
-        (click)="toggleFormDisplay();
-        addMeal(newName.value, newDetails.value, newCalories.value);">
-        Track Meal</div>
+    <div *ngIf="addNewForm" class="new-form">
+      <div class="new-meal-name">
+        <label>Meal Name</label><br>
+        <input #newName type="text"><br>
+      </div>
+      <div class="new-details-name">
+        <label>Details</label><br>
+        <input #newDetails type="text"><br>
+      </div>
+      <div class="new-calories-name">
+        <label>Calories</label><br>
+        <input #newCalories type="number"><br>
+      </div>
+      <div class="new-meal-button">
+        <div class="button"
+          (click)="toggleFormDisplay();
+          addMeal(newName.value, newDetails.value, newCalories.value);">
+          Track Meal</div>
+      </div>
     </div>
   `
 })
